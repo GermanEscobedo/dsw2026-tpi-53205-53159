b94fdf8335d641a0d0fe92c832eef367a1284b8a
GermanEscobedo <Lautaro.Escobedo@alu.frt.utn.edu.ar>
2026-07-18 17:37:15 -0300

    se agrega IPatientService

---
+++ Dsw2026Tpi.Application/Interfaces/IPatientService.cs
@@ -0,0 +1,21 @@
+﻿using Dsw2026Tpi.Application.Dtos;
+
+namespace Dsw2026Tpi.Application.Interfaces;
+
+public interface IPatientService
+{
+    // Lista Completa
+    Task<IEnumerable<PatientModel.Response>> GetAllAsync();
+
+    // Busca 
+    Task<PatientModel.Response?> GetByIdAsync(Guid id);
+
+    // Crea 
+    Task<PatientModel.Response> CreateAsync(PatientModel.Request model);
+
+    // Actualiza 
+    Task UpdateAsync(Guid id, PatientModel.Request model);
+
+    // Elimina 
+    Task DeleteAsync(Guid id);
+}
