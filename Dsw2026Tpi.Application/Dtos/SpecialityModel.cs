3c2fdf67fac37f7df187d5f5c8760af34ba1b8a4
GermanEscobedo <Lautaro.Escobedo@alu.frt.utn.edu.ar>
2026-07-28 13:47:11 -0300

    se agrega SpecialityModel y ISpecialityService

---
+++ Dsw2026Tpi.Application/Dtos/SpecialityModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Dsw2026Tpi.Application.Dtos;
+
+public class SpecialityModel
+{
+    // Datos que pedimos para crear o actualizar una especialidad
+    public record Request(string Name, string Description);
+
+    // Datos que devolvemos al consultar
+    public record Response(Guid Id, string Name, string Description);
+}
---
+++ Dsw2026Tpi.Application/Interfaces/ISpecialityService.cs
@@ -0,0 +1,12 @@
+﻿using Dsw2026Tpi.Application.Dtos;
+
+namespace Dsw2026Tpi.Application.Interfaces;
+
+public interface ISpecialityService
+{
+    Task<IEnumerable<SpecialityModel.Response>> GetAllAsync();
+    Task<SpecialityModel.Response?> GetByIdAsync(Guid id);
+    Task<SpecialityModel.Response> CreateAsync(SpecialityModel.Request model);
+    Task UpdateAsync(Guid id, SpecialityModel.Request model);
+    Task DeleteAsync(Guid id); // Borrado lógico
+}
